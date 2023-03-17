4d004ad85728d2ef5299220a4acd0b73767bbfbe
PCalurie <peterkaruri1999@gmail.com>
2023-03-17 13:07:46 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	for (c = 0 ; c < 10 ; c++)
+	{
+		printf("%i", c);
+	}
+	printf("\n");
+
+return (0);
+}
