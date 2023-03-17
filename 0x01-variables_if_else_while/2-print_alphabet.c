6626da00652484b2fbcdb6eaaeddb6cdce8df731
PCalurie <peterkaruri1999@gmail.com>
2023-03-17 12:52:06 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	for (c = 97 ; c < 123 ; c++)
+	{
+	putchar(c);
+	}
+	for (c = 65 ; c < 91 ; c++)
+	{
+	putchar(c);
+	}
+	putchar('\n');
+
+return (0);
+}
