1f055e287a8a8bca5a638aced0667d8536e337ff
PCalurie <peterkaruri1999@gmail.com>
2023-03-18 08:38:03 -0700

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,15 +8,15 @@ int main(void)
 {
 	int x, y, z;
 
-	for (x = 0; x <= 9; x++) /*hundreds place*/
+	for (x = '0'; x <= '9'; x++) /*hundreds place*/
 	{
 		for (y = (x + 1); y <= '9'; y++) /*tens=100s+1*/
 		{
 			for (z = (y + 1); z <= '9';z++) /*ones*/
 			{
-				putchar(x + '0');
-				putchar(y + '0');
-				putchar(z + '0');
+				putchar(x);
+				putchar(y);
+				putchar(z);
 
 				if (x != '7' || y != '8' || z != '9')
 				{
