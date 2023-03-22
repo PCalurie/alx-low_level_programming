619736f37fb986fa436964d5fc9c2772720d1549
PCalurie <peterkaruri1999@gmail.com>
2023-03-22 02:13:44 -0700

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/*
+ * main -computes the absolute value of an integer.
+ * Return: 0
+ *
+ */
+int _abs(int)
+{
+	if (i >= 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (i * -1);
+	}
+}
