33e7072c643a5a4b1429da2814292a6ffb20b0b6
PCalurie <peterkaruri1999@gmail.com>
2023-03-22 05:06:35 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - prints the sum of two numbers
+ * @a: function parameter 1
+ * @b: function parameter 2
+ * Return: sum
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
