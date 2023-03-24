94573e0e4a867f1b249fd067c5c7e9a298f88f69
PCalurie <peterkaruri1999@gmail.com>
2023-03-24 03:47:29 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * more_numbers - prints 0 to 14, 10 times
+ * Return: returns no value
+ */
+void more_numbers(void)
+{
+	int a, b;
+
+	for (a = 0; a <= 14; a++)
+	{
+		for (b = 0; b < 15; b++)
+		{
+			if (b > 9)
+				_putchar(b / 10 + '0');
+			_putchar(b % 10 + '0');
+		}
+		_putchar('\n');
+	}
+}
