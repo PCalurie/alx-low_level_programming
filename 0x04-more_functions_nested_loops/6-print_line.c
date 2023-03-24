329b1b40e76e14e51f00cc588d510aedc85115af
PCalurie <peterkaruri1999@gmail.com>
2023-03-24 04:14:11 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_line - prints a line
+ * @n: number of _
+ * Return: void
+ */
+
+void print_line(int n);
+{
+	int i;
+
+	for (i = 1, i <= n, i++)
+		_putchar('_');
+	else
+		_putchar('\n');
+}
