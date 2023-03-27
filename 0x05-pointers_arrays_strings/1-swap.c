63b7a8a4b63dc654fb781fd52fedbcb10106b4ae
PCalurie <peterkaruri1999@gmail.com>
2023-03-27 12:58:51 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * void swap_int - swaps values of two pointers
+ * @a: function parameter 1
+ * @b: function parameter 2
+ *
+ * Return: void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c;
+
+	c = *a;
+	*a = *b;
+	*b = c;
+}
