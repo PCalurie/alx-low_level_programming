81bd7872e2375f809d6c89e7303195bfb74f905c
PCalurie <peterkaruri1999@gmail.com>
2023-03-28 09:36:02 -0700

    Generate random 12 character password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -2,31 +2,59 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - Generates a random password consisying of 12 characters
+ * main - Generates a random password consisting of 12 characters
  * Return: Always 0
  */
 
 int main(void)
 {
-	char password[14];
-	int i, random_num;
+    char password[84];  // Define a char array to hold the password
+    int i = 0, j = 0, first, second;
 
-	srand(time(NULL));
+    srand(time(0));  // Seed the random number generator with the current time
 
-	for (i = 0; i < 12; i++)
-	{
-	random_num = rand() % 62;
-	if (random_num < 26)
-		password[i] = 'a' + random_num;
-        else if (random_num < 52)
-            password[i] = 'A' + (random_num - 26);
-        else
-            password[i] = '0' + (random_num - 52);
-	}
-    password[13] = '\0';
+    while (j < 2772)  // Loop until the sum of the ASCII values of the password is 2772
+    {
+        password[i] = 33 + rand() % 94;  // Generate a random character between ASCII 33 and 126
+        j += password[i++];  // Add the ASCII value of the character to the sum and increment the index
+    }
+    
+    password[i] = '\0';  // Terminate the password string with a null character
+    
+    if (j != 272)  // If the sum is not 272 (2772 - 2*750), adjust the first and second characters
+    {
+        first = (j - 2772) / 2;  // Calculate the value to subtract from the first character
+        second = (j - 2772) / 2;  // Calculate the value to subtract from the second character
 
-    printf("Password: %s\n", password);
+        if ((j - 2772) % 2 != 0)  // If the sum is odd, add 1 to the first character adjustment
+        {
+            first++;
+        }
 
-    return 0;
+        // Find the first character greater than or equal to the adjusted value and subtract the adjustment
+        for (i = 0 ; password[i] ; i++)
+        {
+            if (password[i] >= (33 + first))
+            {
+                password[i] -= first;
+                break;
+            }
+        }
+
+        // Find the first character greater than or equal to the adjusted value and subtract the adjustment
+        for (i = 0 ; password[i] ; i++)
+        {
+            if (password[i] >= (33 + second))
+            {
+                password[i] -= second;
+                break;
+            }
+        }
+    }
+
+    printf("%s", password);  // Print the password
+
+    return (0);
 }
+
 
