a1b2a8549735b7fea4520684be4666655afb8c31
PCalurie <peterkaruri1999@gmail.com>
2023-03-28 09:41:29 -0700

    Generate random 12 character password

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,30 +8,30 @@
 
 int main(void)
 {
-    char password[84];  // Define a char array to hold the password
+    char password[84];  /* Define a char array to hold the password*/
     int i = 0, j = 0, first, second;
 
-    srand(time(0));  // Seed the random number generator with the current time
+    srand(time(0));  /* Seed the random number generator with the current time*/
 
-    while (j < 2772)  // Loop until the sum of the ASCII values of the password is 2772
+    while (j < 2772)  /* Loop until the sum of the ASCII values of the password is 2772*/
     {
-        password[i] = 33 + rand() % 94;  // Generate a random character between ASCII 33 and 126
-        j += password[i++];  // Add the ASCII value of the character to the sum and increment the index
+        password[i] = 33 + rand() % 94;  /* Generate a random character between ASCII 33 and 126*/
+        j += password[i++];  /* Add the ASCII value of the character to the sum and increment the index*/
     }
     
-    password[i] = '\0';  // Terminate the password string with a null character
+    password[i] = '\0';  /* Terminate the password string with a null character*/
     
-    if (j != 272)  // If the sum is not 272 (2772 - 2*750), adjust the first and second characters
+    if (j != 272)  /*If the sum is not 272 (2772 - 2*750), adjust the first and second characters*/
     {
-        first = (j - 2772) / 2;  // Calculate the value to subtract from the first character
-        second = (j - 2772) / 2;  // Calculate the value to subtract from the second character
+        first = (j - 2772) / 2;  /* Calculate the value to subtract from the first character*/
+        second = (j - 2772) / 2;  /* Calculate the value to subtract from the second character*/
 
-        if ((j - 2772) % 2 != 0)  // If the sum is odd, add 1 to the first character adjustment
+        if ((j - 2772) % 2 != 0)  /* If the sum is odd, add 1 to the first character adjustment*/
         {
             first++;
         }
 
-        // Find the first character greater than or equal to the adjusted value and subtract the adjustment
+        /* Find the first character greater than or equal to the adjusted value and subtract the adjustment*/
         for (i = 0 ; password[i] ; i++)
         {
             if (password[i] >= (33 + first))
@@ -41,7 +41,7 @@ int main(void)
             }
         }
 
-        // Find the first character greater than or equal to the adjusted value and subtract the adjustment
+        /* Find the first character greater than or equal to the adjusted value and subtract the adjustment*/
         for (i = 0 ; password[i] ; i++)
         {
             if (password[i] >= (33 + second))
@@ -52,7 +52,7 @@ int main(void)
         }
     }
 
-    printf("%s", password);  // Print the password
+    printf("%s", password);  /* Print the password*/
 
     return (0);
 }
