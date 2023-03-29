5019e412e4f7014385587cfb44f73c39f9b7c642
PCalurie <peterkaruri1999@gmail.com>
2023-03-29 10:53:10 -0700

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -6,11 +6,11 @@
  * Return: always 0 success
  */
 
-int _strcmp(char s1, char s2)
+int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	for (int i = 0; s1[i] != '\0'; || s2[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
 		if (s1[i] != s2[i])
 		{
