f5149ba01c7c3bbb2955fcd57e8f4783813e609b
PCalurie <peterkaruri1999@gmail.com>
2023-03-30 07:50:56 -0700

    add two numbers

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,23 +9,17 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int add = 0, len1, len2, i, j;
-
+	int add = 0, len1 = 0, len2 = 0, i = 0, j = 0;
 /* calculate the length of input strings*/
-	for (len1 = 0; n1[len1]; len1++)
+	for (; n1[len1]; len1++)
 		;
-	for (len2 = 0; n2[len2]; len2++)
+	for (; n2[len2]; len2++)
 		;
-
 /* if the result string is less than sum size, return 0 */
 	if (len1 > size_r || len2 > size_r)
 		return (0);
-
 /* decreament the length and size counter as they are 1-indexed */
-	len1--;
-	len2--;
-	size_r--;
-
+	len1--, len2--, size_r--;
 /* iterate through the arrays starting from the end */
 	for (i = 0; i < size_r; i++, len1--, len2--)
 	{
@@ -34,25 +28,19 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 			add += n1[len1] - '0';
 		if (len2 >= 0)
 			add += n2[len2] - '0';
-
 /* if there are no more digits to add and carry is 0, exit loop */
 		if (len1 < 0 && len2 < 0 && add == 0)
 			break;
-
 /* store the sum in the result array */
 		r[i] = add % 10 + '0';
-
 /* calculate the carry to be added in the next iteration */
 		add /= 10;
 	}
-
 /* terminate the result string */
 	r[i] = '\0';
-
-/* if digits still left in the input arrays or carry is not 0, return 0 */
+/* if digits still in input arrays or return 0 */
 	if (len1 >= 0 || len2 >= 0 || add)
 		return (0);
-
 /* reverse the result array */
 	for (i--, j = 0; i > j; i--, j++)
 	{
@@ -60,7 +48,6 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		r[i] = r[j];
 		r[j] = add;
 	}
-
 /* return the result */
 	return (r);
 }
