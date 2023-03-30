2db06538288df6233a36a1a7df806944bd5bbbe8
PCalurie <peterkaruri1999@gmail.com>
2023-03-30 02:16:41 -0700

    Encoding with ROT13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -5,25 +5,19 @@
  *
  * Return: pointer to encoded string
  */
-
 char *rot13(char *s)
 {
 	char rot, *r = s;
-/* iterate through the string */
+
 	while (*s)
 	{
-/* calculate ASCII code of 'A' or 'a' based on case on the character */
 		rot = (*s & 32) + 65;
-
-		/* check if character is a letter */
-		if ((*s >= 'a' && *s <= 'z') || (*s >= 'A' || *s <= 'Z'))
+		if ((*s >= 'a' && *s <= 'z') || (*s >= 'A' && *s <= 'Z'))
 		{
-			/* apply ROT-13 encryption */
 			*s = (*s - rot + 13) % 26 + rot;
 		}
-		/* move to the next character*/
 		s++;
 	}
-	/* return the modified string */
+
 	return (r);
 }
