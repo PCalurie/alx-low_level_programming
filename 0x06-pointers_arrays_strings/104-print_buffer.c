4b587282056bc231e357b238a4288655937cb378
PCalurie <peterkaruri1999@gmail.com>
2023-03-30 09:13:45 -0700

    Print a buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -10,41 +10,44 @@
 
 void print_buffer(char *b, int size)
 {
-	int i, j;
+	int i, j, k;
+
+	k = 0;
 
 	if (size <= 0)
-		printf("\n")
-	else
 	{
-		for (i = 0; i <= size; i += 10)
-		{
+		printf("\n");
+		return;
+	}
 /* print the address of the current line*/
-			prinf("%.8x:", i);
-
+	while (k < size)
+	{
+		j = size - k < 10 ? size - k : 10;
+		printf("%08x: ", k);
 /* print the hexadecimal representation of the bytes */
-			for (j = i; j < i + 10; j++)
-			{
-			if (j % 2 == 0)
-				pfintf(" ");
-
-			if (j < size)
-				printf("%.2x", *(b + j));
+		for (i = 0; i < 10; i++)
+		{
+			if (i < j)
+				printf("%02x", *(b + k + i));
 			else
+				printf("  ");
+			if (i % 2)
+			{
 				printf(" ");
 			}
+		}
 /*print the ASCII representation of the bytes */
-		for (j = i; j < i + 10; j++)
-		{
-			if (j >= size)
-				break;
+			for (i = 0; i < j; i++)
+			{
+				int c = *(b + k + i);
 
-		if (*(b + j) < 32 || *(b + j) > 126)
-			printf("%c", '.');
-		else
-			printf("%c", *(b + j));
-		}
-
-		printf("\n");
-		}
+				if (c < 32 || c > 132)
+				{
+					c = '.';
+				}
+				printf("%c", c);
+			}
+			printf("\n");
+			k += 10;
 	}
 }
