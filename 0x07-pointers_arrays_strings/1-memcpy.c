d156c272c287484aefeb0dd95f7e66ca2d11fc35
PCalurie <peterkaruri1999@gmail.com>
2023-04-03 08:45:49 -0700

    Copy the contents from one buffer into another

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,13 +1,14 @@
 #include "main.h"
 
-/*
- * _memcpy - copies a memory area with a specified value from src file
+/**
+ * _memcpy - copies a memory area with a specified value from src to dest
  * @dest: destination of the copied values
  * @src: source of the values to copy
  * @n: number of bytes to be set to the value dest
+ * Return: dest
  */
 
-char *_memcpy(char *_dest, char *_src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	if (n > 0)
 	{
@@ -20,4 +21,5 @@ char *_memcpy(char *_dest, char *_src, unsigned int n)
 
 		return (dest);
 	}
+	return (dest);
 }
