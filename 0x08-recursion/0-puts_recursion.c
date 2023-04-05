70fcdd96630705cf72b47f89bbe77a1235a5ca97
PCalurie <peterkaruri1999@gmail.com>
2023-04-05 07:23:13 -0700

    Print a string

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -2,15 +2,17 @@
 /**
  * _puts_recursion - prints characters of a string
  * @s: pointer to the string with the characters
- * Return: string
  */
 
 void  _puts_recursion(char *s)
 {
 	if (*s == '\0')
+	{
 		_putchar('\n');
-	return;
+		return;
+	}
 
 	_putchar(*s);
-	_puts_recursion(s++);
+	s++;
+	_puts_recursion(s);
 }
