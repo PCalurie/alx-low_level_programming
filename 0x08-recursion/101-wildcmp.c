41c1a2b55b996d893f6abb839b00f17242c585e0
PCalurie <peterkaruri1999@gmail.com>
2023-04-06 14:59:12 -0700

    compare two strings

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -12,18 +12,18 @@ int wldcmp(char *s1, char *s2)
 	return (1);
 /* if both current char match move to the next char */
 	if (*s1 == *s2)
-	return (wldcmp(s1 +1, s2 +1));
+	return (wldcmp(s1 + 1, s2 + 1));
 /* if the current char in s2 is '*' then move to the next char */
 	if (*s2 == '*')
 	{
 /* if s2 ends with '*', they are identical */
-		if (*(s2 +1) == '\0')
+		if (*(s2 + 1) == '\0')
 		return (1);
 /* compare the rest of s1 with s2 after the '*' */
 		if (wldcmp(s1, s2 + 1))
 		return (1);
 /* compare the rest f s1 starting with the next char with the rest of s2 */
-		if (s1 != '\0' wldcmp(s2 +1, s2))
+		if (s1 != '\0' wldcmp(s2 + 1, s2))
 		return (1);
 	}
 
