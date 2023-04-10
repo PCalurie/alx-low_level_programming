6dfcf258e74c861dd6426575844f9fbd95a3d929
PCalurie <peterkaruri1999@gmail.com>
2023-04-10 08:51:17 -0700

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,9 +8,6 @@
  */
 int (int argc, char *argv[])
 {
-	int amnt;
-	int coins [] = {25, 10, 5, 2, 1};
-	int num_coins, count, x;
 /* check that there is only one argument including the name of prg */
 	if (argc != 2)
 	{
@@ -18,28 +15,29 @@ int (int argc, char *argv[])
 		return (1);
 	}
 /* convert the argument into an int */
-	amnt = atoi(argv[1]);
+	int cents = atoi(argv[1]);
 /* check that the amnt is positive */
 	if (amnt < 0)
 	{
 		printf("0\n");
 		return (0);
+	}
+/* define an array of coin values */
+	int coins[5] = {25, 10, 5, 2, 1};
 /* initialize a counter for the num of coins used */
-	num_coins = sizeof(coins) / sizeof(coins[0]);
-	count = 0;
+	int num_coins = 0;
+	int count;
 /* iterate over the num of coins used */
+	int x;
 
 	for (x = 0; x < 5; x++)
 	{
-		while (amnt > coins[x])
-		{
-/* substract the coin value from the amnt and increament the counter */
-			amnt -= coins[x];
-		count++;
-		}
+		count = coins[x];
+		num_coins += cents / count;
+		cents %= count;
 	}
 
-	printf("%d\n", count);
+	printf("%d\n", num_coins);
 
 	return (0);
 }
--- 0x0A-argc_argv/README.md
+++ 0x0A-argc_argv/README.md
@@ -1,2 +1,7 @@
 # 0x0A-argc_argv
-1. 
+0. A programme that writes its name.
+1. Prints the number of arguments passed to it.
+2. Prints all the arguments. 
+3. A multiplication programme.
+4. An addition programme.
+5. Minimal number of coins to return for a given amnt of money.
