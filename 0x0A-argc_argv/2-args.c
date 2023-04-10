d5d2fa420bffdaf0a1294a57138219f4f5e1b23e
PCalurie <peterkaruri1999@gmail.com>
2023-04-10 05:43:17 -0700

    Prints number of arguments

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints all arguments
+ * @argv: arguments
+ * @argc: number of arguments
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int x = 0;
+
+	while (argc > x)
+		printf("%s\n", argv[x]);
+	x++;
+
+	return (0);
+}
