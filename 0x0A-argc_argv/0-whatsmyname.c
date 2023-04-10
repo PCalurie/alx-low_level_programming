49f5473576e29f3a11461a1e5e7ce3e1b8511a81
PCalurie <peterkaruri1999@gmail.com>
2023-04-10 04:54:36 -0700

    Program name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints a string
+ * @argc: argument count
+ * @argv: argument
+ * Return: 0 success
+ */
+int main(int argc, char *argv[])
+{
+	void argc;
+
+	printf("%s\n", argv[0])
+
+	return (0);
+}
