2917eb88b94b8032b5136c8ab746741f3a76c475
PCalurie <peterkaruri1999@gmail.com>
2023-04-10 13:06:57 -0700

    static library

---
+++ 0x09-static_libraries/100-atoi.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * _atoi - prints digit
+ * @s: string
+ * Return: string as digit
+ */
+int _atoi(char *s)
+{
+	return atoi(s);
+}
