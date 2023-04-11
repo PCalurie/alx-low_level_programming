568baa1499cf5bfc5bcb17395de2701500d502d0
PCalurie <peterkaruri1999@gmail.com>
2023-04-11 13:51:57 -0700

    prints a 2D grid of integers

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * alloc_grid - creates two dimensional grid of integers with
+ * specified height and width
+ * @width: with of integers
+ * @height: height of integers
+ * Return: a grid
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (NULL);
+
+	grind = malloc(sizeof(int *) * height)
+		if (grid == NULL)
+			return (NULL);
+
+	for (i = 0; i < height; i++)
+	{
+		grind[i] = malloc(sizeof(int *) * width)
+			if (grind[i] == NULL)
+			{
+				for (j = 0; j < i; j++)
+					free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+	}
+
+	for (i = 0; i < height; i++)
+	{
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return (grid);
+}
