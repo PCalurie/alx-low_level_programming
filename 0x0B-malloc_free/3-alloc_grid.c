481e290c0e03f43cd998880ea966d2bb22ad2feb
PCalurie <peterkaruri1999@gmail.com>
2023-04-11 14:35:01 -0700

    prints a 2D grid of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -27,15 +27,16 @@ int **alloc_grid(int width, int height)
 			{
 				for (j = 0; j < i; j++)
 					free(grid[j]);
-			}
+
 			free(grid);
 			return (NULL);
-	}
+			}
 
 	for (j = 0; j < width; j++)
 		{
 			grid[i][j] = 0;
 		}
+	}
 
 	return (grid);
 }
