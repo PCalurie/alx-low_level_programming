a110d2dcf37f81ac4f024039add8db01f5129c27
PCalurie <peterkaruri1999@gmail.com>
2023-04-12 11:34:57 -0700

    split str into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,2 +1,101 @@
 #include "main.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/* helper functions */
+int word_len(char *str);
+int count_words(char *str);
 
+int idx, words, letters, w, l;
+/**
+ * word_len - finds the first word of the string
+ * @str: the string to check in
+ * Return: the index marking the end of the word
+ */
+int word_len(char *str)
+{
+	int index = 0, len = 0;
+
+	while (*(str + index) && *(str + index) != ' ')
+	{
+		len++;
+		index++;
+	}
+
+	return (len);
+}
+/**
+ * count_words - counts the words in the str
+ * @str: the string to count words in
+ * Return: number of words in the str
+ */
+int count_words(char *str)
+{
+	int words = 0, len = 0, index = 0;
+
+	for (index = 0; *(str + index); index++)
+		len++;
+
+	for (index = 0; index < len; index++)
+	{
+		if (*(str + index) != ' ')
+		{
+			words++;
+			index += word_len(str + index);
+		}
+	}
+
+	return (words);
+}
+
+/**
+ * strtow - function splits a string into two words
+ * @str: string to split
+ * Return: split words
+ */
+char **strtow(char *str)
+{
+	char **strings;
+
+	if (str == NULL || str[0] == '\0')
+		return (NULL);
+
+	words = count_words(str);
+	if (words == 0)
+		return (NULL);
+
+/* allocate memory for the array of string */
+	strings = malloc(sizeof(char *) * (words + 1));
+	if (strings == NULL)
+		return (NULL);
+
+/* loop through string and count words */
+	for (w = 0, (idx = 0); w < words; w++)
+	{
+		while ((str[idx] = ' '))
+			idx++;
+
+		letters = word_len(str + idx);
+
+/* allocate memory for the current word */
+		strings[w] = malloc(sizeof(char) * (letters + 1));
+
+		if (strings[w] == NULL)
+		{
+			for (w--; w >= 0; w--)
+				free(strings[w]);
+
+			free(strings);
+			return (NULL);
+		}
+/* copy current words into array */
+		for (l = 0; l < letters; l++)
+			strings[w][l] = str[idx++];
+
+		strings[w][l] = '\0';
+	}
+
+	strings[w] = NULL;
+
+	return (strings);
+}
