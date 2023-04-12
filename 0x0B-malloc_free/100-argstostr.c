b1454dc1c273f565b5107786a9d31cb836c2b686
PCalurie <peterkaruri1999@gmail.com>
2023-04-12 01:37:25 -0700

    Concatenates all arguments passed

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * argstostr - cocatenates all arguments given
+ * @ac: number of arguments
+ * @av: arguments
+ * Return: pointer to new str
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, j, len = 0, n = 0;
+	char *str;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+
+/* calculate the length of inputed str */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			len++;
+		len++; /* for the new line */
+		}
+	}
+/* allocate mem for the new char */
+	str = malloc((sizeof(char) * len) + 1);
+	if (str == NULL)
+		return (NULL);
+/* copy the argument to the new str */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0')
+		{
+			str[n] = av[i][j];
+			n++;
+		}
+		str[n] = '\n' /* add new line */
+		n++;
+	}
+	str[n] = '\0' /* add null terminator */
+	
+	return (str);
+}
--- 0x0B-malloc_free/README.md
+++ 0x0B-malloc_free/README.md
@@ -1,5 +1,6 @@
 # 0x0B-malloc_free
 1. **"create_array"** creates an array of chars, and initializes it with a specific char. This function takes in two parameters: the size of the array to create and the character with which to fill the array. It returns a pointer to the array.
 2. **"_strdup"** returns a pointer to a newly allocated space in memory which contains a copy of the string given as a parameter.
-3. **"_strcocat"** cocatenates two strings into one.
+3. **"_strcocat"** concatenates two strings into one.
 4. **"alloc_grid"** prints a 2D grid of integers.
+5. **"argstostr"** concatenates all arguments passed.
