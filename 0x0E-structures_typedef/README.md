94d64e9734ce737d9b9aa397bea5194a7ab3dcd2
PCalurie <peterkaruri1999@gmail.com>
2023-04-17 06:59:38 -0700

    Tommy's header file

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,8 @@
+# 0x0E-structures_typedef
+1. Define a new type struct dog with the following elements:
+,,,
+name, type = char *
+age, type = float
+owner, type = char * 
+,,,
+2. 
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog's profile
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+};
+/* Tommy - typedef for struct dog */
+typedef struct dog Tommy;
+
+#endif
