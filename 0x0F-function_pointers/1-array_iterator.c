684d35019316aa204bcf800be0c1187d0da0d47b
PCalurie <peterkaruri1999@gmail.com>
2023-04-19 09:10:42 -0700

    executing a function parameter

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,9 +11,9 @@ void array_iterator(int *array, size_t size, void(*action)(int))
 {
 	size_t a;
 
-	if (!array || !action)
-		return;
-
+	if (array != NULL && action != NULL && size > 0)
+	{
 	for (a = 0; a < size; a++)
 		action(array[a]);
+	}
 }
