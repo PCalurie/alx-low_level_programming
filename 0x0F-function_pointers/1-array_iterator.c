59ba5f5d9c2fc391275ce08d4e47ce92a7ba8d8b
PCalurie <peterkaruri1999@gmail.com>
2023-04-19 13:09:33 -0700

    array iterator

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -14,7 +14,7 @@ void array_iterator(int *array, size_t size, void(*action)(int))
 
 	if (array != NULL && action != NULL && size > 0)
 	{
-	for (a = 0; a <= size; a++)
+	for (a = 0; a < size; a++)
 		action(array[a]);
 	}
 }
