4b8bd155fc7eb1076b6407fd9d48c2e5d4321d55
PCalurie <peterkaruri1999@gmail.com>
2023-04-20 07:55:57 -0700

    print integers sepearated by a str

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,4 +1,4 @@
-#include "variadic_function.h"
+#include "variadic_functions.h"
 #include <stdio.h>
 #include <stdarg.h>
 /**
@@ -17,8 +17,8 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		printf("%d", va_arg(digits, int));
 
-		if (i != (n - 1) && seperator != NULL)
-			printf("%s", seperator);
+		if (i != (n - 1) && separator != NULL)
+			printf("%s", separator);
 	}
 	printf("\n");
 
