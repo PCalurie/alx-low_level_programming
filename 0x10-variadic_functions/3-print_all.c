4eb39dc76c1b77dd4fd3f4e786e3ee7251b1103c
PCalurie <peterkaruri1999@gmail.com>
2023-04-20 14:04:24 -0700

    prints anything

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,5 +1,7 @@
 #include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 /**
  * print_all - function prints all data types
  * @format: format to print
@@ -8,45 +10,39 @@ void print_all(const char * const format, ...)
 {
 	int i;
 	char *str;
-	va_list all;
+	char *separator = "";
 
+	va_list all;
 	va_start(all, format);
+
 	for (i = 0; format && format[i]; i++)
 	{
 		switch (format[i])
 		{
-			if (format[i] == 'c')
-			{
-				all = va_arg(args, int);
-				printf("%c", all);
-			}
-			else if (format[i] == 'i')
-			{
-				all = va_arg(args, int);
-				printf("%d", all);
-			}
-			else if (format[i] == 'f')
-			{
-				all = va_arg(args, double);
-				printf("%f", all);
-			}
-			else if (format[i] == 's')
-			{
-				str = va_arg(args, char *);
+			case 'c':
+				printf("%s%c", separator, va_arg(all, int));
+				break;
+			case 'i':
+				printf("%s%d", separator, va_arg(all, int));
+				break;
+			case 'f':
+				printf("%s%f", separator, va_arg(all, double));
+				break;
+			case 's':
+				str = va_arg(all, char *);
 
 				if (str == NULL)
 					str = "(nil)";
 
-			printf("%s", all);
-			}
+				printf("%s%s", separator, str);
+				break;
+			default:
+				i++;
+				continue;
 		}
-		if ((format[i + 1] && strchr("cifs", format[i]))
-			printf("%s", separator);
-			separator = ", ";
-		i++;
-
+		separator = ", ";
 	}
-	printf("\n");
 
-	va_ends(all);
+	va_end(all);
+	printf("\n");
 }
