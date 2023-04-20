afebac4728d3a35bdb940a8a23b5331a4bcf47d7
PCalurie <peterkaruri1999@gmail.com>
2023-04-20 07:52:16 -0700

    print integers sepearated by a str

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		if (i != (n - 1) && seperator != NULL)
 			printf("%s", seperator);
 	}
-	printf('\n');
+	printf("\n");
 
 	va_end(digits);
 }
