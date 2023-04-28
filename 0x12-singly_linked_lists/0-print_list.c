76923ad364900b3c52a3ce266a7461fcc62a2087
PCalurie <peterkaruri1999@gmail.com>
2023-04-28 01:19:12 -0700

    prints all elements in the list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,6 +1,7 @@
 #include "lists.h"
 #include <stdio.h>
-
+#include <stddef.h>
+#include <stdlib.h>
 /**
  * print_list - prints all the elements of a list_t list
  * @h: pointer to the head of the list
@@ -18,7 +19,7 @@ size_t print_list(const list_t *h)
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
 
 		/* increment the count */
 		count++;
