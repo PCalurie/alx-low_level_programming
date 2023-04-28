4ebe1b09b95da919af7f9fdedc7dcedbf9996770
PCalurie <peterkaruri1999@gmail.com>
2023-04-28 06:39:42 -0700

    assembly lng

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,12 +1,19 @@
-section .data
-    msg db 'Hello, Holberton', 10, 0
+; extern printf: tells the linker that the symbol printf is defined elsewhere
 
-section .text
-    global main
+extern printf
+	section .data
+msg:	 db "Hello, Holberton", 0 ; string to be printed
+fmt:	 db "%s", 10, 0 ; format string for printf
 
+	section .text
+	global main
 main:
-    mov rax, 0
-    mov rdi, msg
-    call printf
-    mov rax, 0
-    ret
+	push rbp ; save the base pointer of the previous stack frame
+	mov rdi,fmt ; move the address of format string into the first argument register (rdi)
+	mov rsi,msg ; move the address of message into the second argument register (rsi)
+	mov rax,0 ; set return value to 0
+	call printf ; call the printf function with the given arguments
+
+	pop rbp ; restore the base pointer of the previous stack frame
+	mov rax,0 ; set return value to 0
+	ret ; return from the function
