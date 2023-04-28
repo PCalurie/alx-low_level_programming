9342b8cd7cf82b939f088c849a17b1dfcd48e209
PCalurie <peterkaruri1999@gmail.com>
2023-04-28 01:47:19 -0700

    prints all elements in a list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -18,7 +18,7 @@ size_t print_list(const list_t *h)
 	{
 		/* print length and string value of the current node */
 		if (c_node->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%d] %s\n", 0, "(nil)");
 		else
 			printf("[%d] %s\n", c_node->len, c_node->str);
 
