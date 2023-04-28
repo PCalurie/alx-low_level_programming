81f21e51f6a445ceb71ac04b678cf32c52033ee4
PCalurie <peterkaruri1999@gmail.com>
2023-04-28 04:22:25 -0700

    add a node at the end

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,51 +1,50 @@
 #include "lists.h"
 #include <stdlib.h>
 #include <string.h>
-
 /**
- * add_node_end - adds a new node at the end of a list_t list
- * @head: pointer to a pointer to the first node of the list
- * @str: string to be added as the value of the new node
+ * add_node_end - adds a new node to the end of a linked list
+ * @head: pointer to the head of the list
+ * @str: string to be stored in the new node
  *
- * Return: the address of the new node, or NULL if it failed
+ * Return: the address of the new element, or NULL if it failed
  */
-
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_node, *last_node;
-	size_t len = 0;
+	list_t *new_node, *last_node; /* declare a pointer to the nodes */
+	int len = 0; /* initialize length to zero */
 
 /* allocate memory for the new node */
 	new_node = malloc(sizeof(list_t));
 	if (new_node == NULL)
 		return (NULL);
 
-/* duplicate the string to be added as the value of the new node */
+/* copy the string to the new node */
 	new_node->str = strdup(str);
 	if (new_node->str == NULL)
 		free(new_node);
-			return (NULL);
+		return (NULL);
 
-/* calculate the length of the string */
+/* compute the length of the string */
 	while (str[len] != '\0')
 		len++;
 
-/* set the length and next fields of the new node */
+/* store the length and set the next pointer to NULL */
 	new_node->len = len;
 	new_node->next = NULL;
 
-/* add the new node at the end of the list */
+/* if the list is empty, set the head to the new node */
 	if (*head == NULL)
 	{
-/* if the list is empty, make the new node the first */
 		*head = new_node;
 	}
 	else
+/* otherwise, find the last node and set its next pointer to the new node */
 	{
-/* otherwise, find the last node and add the new node after it */
 		last_node = *head;
 		while (last_node->next != NULL)
+		{
 			last_node = last_node->next;
+		}
 		last_node->next = new_node;
 	}
 
