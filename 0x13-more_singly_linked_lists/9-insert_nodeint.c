161e57ac3c926deca06e4625ee063decad11b8a0
PCalurie <peterkaruri1999@gmail.com>
2023-05-02 05:53:48 -0700

    fix a node at n

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,10 +8,10 @@
  */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t new_node, current_node, next_node;
+	listint_t new_node, current_node;
 	unsigned int d;
 
-	if (head == NULL)
+	if (head == NULL || idx != 0)
 		return (NULL);
 
 	new_node = malloc(sizeof(listint_t));
@@ -25,7 +25,6 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		*head = new_node;
 		return (new_node);
 	}
-
 	current_node = *head;
 	for (d = 0; d < idx - 1 && current_node != NULL; d++)
 		current_node = current_node->next;
@@ -35,10 +34,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		free(new_node);
 		return (NULL);
 	}
-
-	next_node = current_node->next;
+	new_node = current_node->next;
 	current_node->next = new_node;
-	new_node->next = next_node;
 
 	return (new_node);
 }
