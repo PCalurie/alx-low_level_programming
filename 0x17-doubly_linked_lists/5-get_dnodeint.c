ecc66da43f96e1b8228686339b853d44370d7c88
PCalurie <peterkaruri1999@gmail.com>
2023-06-16 05:44:18 -0700

    frees a dlistint_t list.

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - function that returns the nth node
+ * of a dlistint_t linked list
+ * @head: pointer to the head node
+ * @index: index of the node, starting from 0
+ * Returb: nth node or NULL if it does not exist
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	while (*head != NULL)
+	{
+		if (count == index)
+			return (head);
+		count++;
+		head = head->next;
+	}
+	return (NULL);
+}
